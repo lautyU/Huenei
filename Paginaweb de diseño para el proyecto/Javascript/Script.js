02f1ccacbd480e5a7d64f2f787db57051cbcf41f
lautyU <47756293+lautyU@users.noreply.github.com>
2020-04-29 02:29:44 -0300

    pagina web

---
+++ Paginaweb de diseño para el proyecto/Javascript/Script.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName('btn_submit')[0].addEventListener("click", aceptar);
+
+function aceptar(){
+
+  alert("cambios guardados");
+}
+document.getElementsByClassName('btn_reset')[0].addEventListener("click",eliminar);
+
+function eliminar(){
+
+alert("se elimino el producto");
+}
